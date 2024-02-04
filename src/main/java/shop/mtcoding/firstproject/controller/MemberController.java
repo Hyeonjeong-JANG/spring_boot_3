33517a7d57a42a6d49abd0bb1633908366737312
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-02-04 16:55:09 +0900

    멤버 등록, 조회, 전체 조회

--- src/main/java/shop/mtcoding/firstproject/controller/MemberController.java
+++ src/main/java/shop/mtcoding/firstproject/controller/MemberController.java
@@ -3,11 +3,15 @@ package shop.mtcoding.firstproject.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import shop.mtcoding.firstproject.dto.MemberForm;
 import shop.mtcoding.firstproject.entity.Member;
 import shop.mtcoding.firstproject.repository.MemberRepository;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
@@ -31,5 +35,19 @@ public class MemberController {
         log.info(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public   String index(Model model){
+        ArrayList<Member> memberEntityList= memberRepository.findAll();
+        model.addAttribute("memberList", memberEntityList);
+        return "members/index";
     }
 }
--- src/main/java/shop/mtcoding/firstproject/repository/MemberRepository.java
+++ src/main/java/shop/mtcoding/firstproject/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package shop.mtcoding.firstproject.repository;
 import org.springframework.data.repository.CrudRepository;
 import shop.mtcoding.firstproject.entity.Member;
 
+import java.util.ArrayList;
+
 public interface MemberRepository extends CrudRepository<Member, Long> {
+    @Override
+    ArrayList<Member> findAll();
 }
